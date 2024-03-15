608bb1670fbd89f691c06f842f0b6127b08839ce
adithyapuvvada <138282999+adithyapuvvada@users.noreply.github.com>
2024-03-15 08:25:31 +0530

    Create testbench.sv

---
+++ #15 priority Encoder/testbench.sv
@@ -0,0 +1,16 @@
+
+module tb;
+  reg [3:0]a;
+  wire [1:0]y;
+  priencoder priencoder1(a,y);
+  initial
+    begin
+      $monitor("a=%b, y=%b",a,y);
+      a=4'b0000;#1
+      a=4'b0001;#1
+      a=4'b0010;#1
+      a=4'b0111;#1
+      a=4'b1000;#1
+      $finish;
+    end
+endmodule
